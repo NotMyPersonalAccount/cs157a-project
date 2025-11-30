86862705e1fd3720a904720e184b942f66bf5a6a
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-11-30 04:04:56 -0800

    Connect to MariaDB server

--- api/main.py
+++ api/main.py
@@ -1,4 +1,22 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+import mariadb
+import os
+import sys
+
+load_dotenv()
+
+try:
+    conn = mariadb.connect(
+        user=os.getenv("MARIADB_USER"),
+        password=os.getenv("MARIADB_PASSWORD"),
+        host=os.getenv("MARIADB_HOST"),
+        port=int(os.getenv("MARIADB_PORT")),
+        database=os.getenv("MARIADB_DATABASE")
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
 app = FastAPI()
 
