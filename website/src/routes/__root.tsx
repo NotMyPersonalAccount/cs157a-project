8338a34d5bce548013be491e9a231af21a93ab42
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-11-30 05:04:32 -0800

    Set up Tanstack Router & Tanstack Query

---
+++ website/src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+const queryClient = new QueryClient();
+
+function RootComponent() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </>
+  );
+}
