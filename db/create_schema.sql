56f8d9f6a50ffbd14a9253469798e0d316408586
Irving Bautista <irvingbautista681@gmail.com>
2025-12-02 14:05:22 -0800

    fix bug

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -34,19 +34,6 @@ CREATE TABLE movie_people (
 
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-<<<<<<< HEAD
-    username VARCHAR(20),
-    active BOOLEAN,
-);
-
-CREATE TABLE reviews (
-    user_id INTEGER FOREIGN KEY (users) REFERENCES users(id) ON DELETE CASCADE,
-    movie_id INTEGER FOREIGN KEY (movies) REFERENCES movies(id) ON DELETE CASCADE,
-    review_text TEXT,
-    rating INTEGER,
-    favorite BOOLEAN,
-    PRIMARY KEY (user_id, movie_id)
-=======
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
@@ -58,5 +45,4 @@ CREATE TABLE favorites (
     UNIQUE KEY unique_fav (user_id, movie_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
->>>>>>> 34f531a (add user authentication, users and favorites table, login/signup page)
 );
