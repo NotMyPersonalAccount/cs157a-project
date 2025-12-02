4d780b9e9f3a41a7ca3345f9dabc7ad6ce1daed9
Saragon4005 <raven.keszei@gmail.com>
2025-12-02 13:05:14 -0800

    add user and review tables

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -7,7 +7,6 @@ CREATE TABLE movies (
     rating VARCHAR(16),
     genre VARCHAR(32),
     image_url VARCHAR(512),
-
     PRIMARY KEY (id)
 );
 
@@ -17,7 +16,6 @@ CREATE TABLE people (
     birth_date DATE,
     biography TEXT,
     image_url VARCHAR(512),
-    
     PRIMARY KEY (id)
 );
 
@@ -27,10 +25,24 @@ CREATE TABLE movie_people (
     person_id INTEGER NOT NULL,
     role VARCHAR(32) NOT NULL,
     character_name VARCHAR(256),
-    
     PRIMARY KEY (id),
     FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
     FOREIGN KEY (person_id) REFERENCES people(id) ON DELETE CASCADE,
     INDEX idx_movie_id (movie_id),
     INDEX idx_person_id (person_id)
+);
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20),
+    active BOOLEAN,
+);
+
+CREATE TABLE reviews (
+    user_id INTEGER FOREIGN KEY (users) REFERENCES users(id) ON DELETE CASCADE,
+    movie_id INTEGER FOREIGN KEY (movies) REFERENCES movies(id) ON DELETE CASCADE,
+    review_text TEXT,
+    rating INTEGER,
+    favorite BOOLEAN,
+    PRIMARY KEY (user_id, movie_id)
 );
