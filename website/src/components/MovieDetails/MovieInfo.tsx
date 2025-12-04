04248100e6711f81030f828d255d83860deb6afb
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-12-03 21:51:42 -0800

    Show movie runtime as hours+mins

--- website/src/components/MovieDetails/MovieInfo.tsx
+++ website/src/components/MovieDetails/MovieInfo.tsx
@@ -4,10 +4,16 @@ interface MovieInfoProps {
 }
 
 export default function MovieInfo({ rating, runtime }: MovieInfoProps) {
+  const runtimeHours = Math.floor(runtime / 60);
+  const runtimeMinutes = runtime % 60;
+
   return (
     <div className="space-y-2 w-[400px]">
       <p className="font-bold text-white text-4xl m-0">{rating}</p>
-      <p className="font-bold text-white text-2xl">{runtime} min</p>
+      <p className="font-bold text-white text-2xl">
+        {!runtimeHours ? "" : `${runtimeHours} h `}
+        {runtimeMinutes} m
+      </p>
     </div>
   );
 }
